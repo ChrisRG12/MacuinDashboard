ed9c6c84bbe7ca6994c1aa15216164d1398b06f8
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-14 12:34:13 -0600

    Creamos la plantilla y agregamos la vista en el web, y le agregamos la tarjeta para poder ingresar los datos del usuario

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,25 +2,33 @@
 
 @section('contenido')
 
+<div class="container">
+
+    <div class="card text-center">
+
+        <div class="card-header">
+          LOGIN
+        </div>
+
+        <div class="card-body">
+            <div class="mb-3">
+                <label class="form-label"> Usuario </label>
+                <input type="text" class="form-control" name="txtusuario">
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label"> Contraseña </label>
+                <input type="text" class="form-control" name="txtusuario">
+            </div>
+
+        </div>
+
+        <div class="card-footer">
+            <a href="#" class="btn btn-primary">Ingresar</a>
+        </div>
+      </div>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</div>
 @stop
