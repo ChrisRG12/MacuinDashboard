bf819fe1186b21a38ef777fa9253ebf36c1b8606
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-14 19:39:39 -0600

    ya quedo el menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,21 +2,23 @@
 
 @section('contenido')
 
+<h1 class="mt-4 text-center" style="color:black"> Macuin Dashboard </h1>
 
-<div id="carouselExampleInterval" class="carousel slide mt-4" data-bs-ride="carousel">
+
+<div id="carouselExampleInterval" class="carousel slide mt-4 col-md-9" data-bs-ride="carousel" style="margin-left: 14%">
 
     <div class="carousel-inner">
 
-      <div class="carousel-item active" data-bs-interval="10000">
-        <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-item active" data-bs-interval="100">
+        <img src="\ima\La.png" class="d-block w-100" alt="...">
       </div>
 
-      <div class="carousel-item" data-bs-interval="2000">
-        <img src="\ima\...jpg" class="d-block w-100" alt="...">
+      <div class="carousel-item" data-bs-interval="200">
+        <img src="\ima\com.jpg" class="d-block w-100" alt="...">
       </div>
 
-      <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-item" data-bs-interval="200">
+        <img src="\ima\conv.jpg" class="d-block w-100" alt="...">
       </div>
 
     </div>
