a8337c6fb371c712d66a990777c76f42fc987a31
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-16 15:22:48 -0600

    Cambios al login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,43 +2,45 @@
 
 @section('contenido')
 
-<div class="container mb-3 mt-4 col-md-6 text-center">
-
-    <h1><i class="bi bi-door-open-fill"></i></h1>
-
-    <div class="card text-center">
-
-        <div class="card-header">
-          LOGIN
-        </div>
-
-        <div class="card-body">
-            
-            <div class="mb-3">
-                
-                <label class="form-label"> Usuario </label>
-                
-                <input type="text" class="form-control" name="txtusuario">
-            </div>
-
-            <div class="mb-3">
-                <label class="form-label"> Contraseña </label>
-                <input type="text" class="form-control" name="txtcontra">
-            </div>
-
-            <a href="#" class="btn btn-success btn-lg"><i class="bi bi-box-arrow-in-right"></i> Ingresar</a>
-
-        </div>
-
-        <div class="card-footer">
-            <a href="#" class="btn btn-secondary"> <i class="bi bi-person-add"></i>  Agregar Usuario</a>
-           
-        </div>
-
-
+<div class="container w-75 mt-5 rounded shadow">
+    <div class="row align-items-stretch">
+      <!--md mediano, lg largo, xl extralargo-->
+      
+      <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 ">
+        <img src="\ima\conv.jpg" width="800" height="700" alt="">
       </div>
+  
+      <div class="col bg-white p-5 rounded-end">
+        <div class="px-lg-5 pt-lg-4 pb-lg-3 w-100 border-0" id="imagen" style="display: flex; aling-items: center; justify-content: center;">
+            <img src="\ima\conv.jpg" width="150" height="100" alt="">
+          </div>
+          <h2 class="fw-bold text-center pt-3 mb-5 ">Iniciar Sesion</h2>
 
+          <!--Form-->
+  
+          <form class="form-signin" action="#" method="post">
 
+              <div class="mb-4">
+                <label for="text" class="form-label">Usuario</label>
+                <input type="text" class="form-control" name="txtuser" required>
+              </div>
 
-</div>
+              <div class="mb-4">
+                <label for="password" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" name="txtcontra" required>
+              </div>
+
+              <div class="d-grid">
+                <button type="submit" class="btn btn-dark">Iniciar Sesion</button>
+              </div>
+
+              <div class="my-3">
+                <span>¿No tienes cuenta?<a href="#" class="nav-link">Registrarte</a></span>
+              </div>
+  
+          </form>
+  
+      </div>
+    </div>
+  </div> 
 @stop
