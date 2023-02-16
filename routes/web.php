56ea2208f7c29ff4afe3664fd2e62117e47f6381
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-16 12:06:18 -0600

    agregamos las rutas para el CRUD de usuarios

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,20 @@ Route::get('Welcome', function () {
 Route::view('plan', 'plantilla')-> name('pla');
 Route::view('/', 'login')-> name('log');
 Route::view('menu', 'menu')-> name('men');
-
-
 Route::view('Usua', 'usuarios')-> name('usu');
+
+//------------------USUARIOS----------------------------
+//Create
+Route::get('Autor/create', [ControladorUsuarios::class, 'create'])->name('Autor.create');
+//store
+Route::post('Autor', [ControladorUsuarios::class, 'store'])->name('Autor.store');
+//index
+Route::get('VistaAutor', [ControladorUsuarios::class, 'index'])->name('Autor.index');
+//edit
+Route::get('Autor/{id}/edit', [ControladorUsuarios::class, 'edit'])->name('Autor.edit');
+//update
+Route::put('Autor/{id}', [ControladorUsuarios::class, 'update'])->name('Autor.update');
+//show
+Route::get('Eliminar/{id}/show', [ControladorUsuarios::class, 'show']) -> name('Eliminar.show');
+//destroy
+Route::delete('Autor/{id}', [ControladorUsuarios::class, 'destroy']) -> name('Autor.destroy');
