fbbfad5a8910f504281a81f548e4f936abcbce85
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-16 12:11:17 -0600

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::view('/', 'login')-> name('log');
 Route::view('menu', 'menu')-> name('men');
 Route::view('Usua', 'usuarios')-> name('usu');
 
+<<<<<<< HEAD
 //------------------USUARIOS----------------------------
 //Create
 Route::get('Autor/create', [ControladorUsuarios::class, 'create'])->name('Autor.create');
@@ -41,3 +42,28 @@ Route::put('Autor/{id}', [ControladorUsuarios::class, 'update'])->name('Autor.up
 Route::get('Eliminar/{id}/show', [ControladorUsuarios::class, 'show']) -> name('Eliminar.show');
 //destroy
 Route::delete('Autor/{id}', [ControladorUsuarios::class, 'destroy']) -> name('Autor.destroy');
+=======
+
+Route::view('Usua', 'usuarios')-> name('usu');
+
+
+
+Route::get('/',[controladorVistas::class,'showHome'])->name('ApodoHome');
+Route::get('/',[controladorBD::class,'create'])->name('contacto.create');
+
+
+
+Route::get('Contacto',[controladorBD::class,'index'])->name('contacto.index');
+
+Route::get('contacto/{id}/edit',[controladorBD::class,'edit'])->name('contacto.edit');
+
+//store
+Route::post('contacto', [controladorBD::class,'store'])->name('contacto.store');
+
+Route::put('contacto/{id}',[controladorBD::class,'update'])->name('contacto.update');
+
+
+Route::get('contacto/{id}/show',[controladorBD::class,'show'])->name('contacto.show');
+
+Route::delete('contacto/{id}',[controladorBD::class,'destroy'])->name('contacto.destroy');
+>>>>>>> 04216e4f30c0501a93c9f916ec902ecc4205fe8b
