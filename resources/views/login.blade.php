b532e04465751375ec39018d8c2a83a9b2878fa0
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-02-24 21:02:31 -0600

    Mejoras al login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,11 +8,13 @@
       
       <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 " style="display: flex; aling-items: center; justify-content: center;">
         <img src="\ima\blac.png" width="690" height="700" alt="">
+        {{-- <video src="\ima\vidd.mp4" width="730" height="600" alt="60" autoplay muted loop></video> --}}
       </div>
   
       <div class="col bg-white p-5 rounded-end" style="color: rgb(57, 36, 58)">
         <div class="px-lg-5 pt-lg-4 pb-lg-3 w-100 border-0" id="imagen" style="display: flex; aling-items: center; justify-content: center;">
-            <img src="\ima\Mac.png" width="150" height="120" alt="">
+            {{-- <img src="\ima\Mac.png" width="150" height="120" alt=""> --}}
+            <video src="\ima\vidd.mp4" width="150" height="120" autoplay muted loop></video>
           </div>
           <h2 class="fw-bold text-center pt-3 mb-5 ">Iniciar Sesion</h2>
 
