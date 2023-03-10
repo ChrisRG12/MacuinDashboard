f55cb182ab07d3377daf4e4371de81eb49eda59a
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2023-03-09 19:34:04 -0600

    Menu Cliente

--- resources/views/MenuCliente.blade.php
+++ resources/views/MenuCliente.blade.php
@@ -25,40 +25,28 @@
 
 @include('ModalPerfilCliente')
 
-{{-- <div class="container mb-3 mt-4 col-md-3">
+<div class="container py-4" style="margin-left: 20%">
 
-
-    <div class="card text-center">
-
-        <div class="card-header">
-          {{Auth::user()->name}}
-        </div>
-
-        <div class="card-body">
-            
-            <div class="mb-3">
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ModalPerfilCliente"> 
-                Perfil
-               </button>
-            {{-- <a type="button"  href="{{route('Usuario.create')}}" class="btn btn-primary">Configuracion de perfil</a>
-            </div>
-
-            <div class="mb-3">
-            <a type="button"  href="{{route('ticket.create')}}" class="btn btn-primary">Generar ticket</a>
-
-            </div>
-
-            <div class="mb-3">
-                <button type="button" class="btn btn-warning">Mis tickets</button>
-            </div>
-
-            <a href="{{route('Cierra.Sesion')}}">
-              <button class="btn btn-danger">Cerrar sesión</button></a>
-
-        </div>
-
+  <div class="row align-items-md-stretch">
+    <div class="col-md-6">
+      <div class="h-100 p-5 text-bg-dark rounded-3">
+        <h2>Registro de Departamentos</h2>
+        <p>Opción que permite registrar nuevos departamentos en la empresa y asignar empleados a ellos. Esta función es útil para mantener un registro organizado de los diferentes departamentos de la empresa y los empleados que trabajan en cada uno de ellos.</p>
+    <a href="{{route('depa.index')}}" class="btn btn-outline-light"><i class="fas fa-building"></i> Registrar</a>
       </div>
+    </div>
+    <div class="col-md-6">
+      <div class="h-100 p-5 bg-light border rounded-3">
+        <h2>Administración de Tickets</h2>
+        <p>Opción para gestionar y resolver problemas y solicitudes de los clientes de la empresa. Permite asignar tickets a los miembros del equipo, hacer seguimiento y dar solución a los problemas.</p>
+    <a href="{{route('ticket.index')}}" class="btn btn-outline-secondary"><i class="fas fa-ticket-alt"></i> Administrar</a>
+      </div>
+    </div>
+  </div>
 
-</div> --}}
+  <footer class="pt-3 mt-4 text-white border-top">
+   Macuin Dashboard © 2022 <br> by: Jelos, Agus y Chris
+  </footer>
+</div>
 
 @stop
