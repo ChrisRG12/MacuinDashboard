fcb07dc837b07c9a7ac808e9d120059dbc7c7448
Agus737 <116747072+Agus737@users.noreply.github.com>
2023-03-13 15:43:20 -0600

    si
    cosas extra

--- resources/views/MenuAuxiliar.blade.php
+++ resources/views/MenuAuxiliar.blade.php
@@ -183,26 +183,6 @@ body{
 </style>
 
 
- <div class="container mb-3 mt-4 col-md-3">
 
-
-    <div class="card text-center">
-
-        <div class="card-header">
-          {{Auth::user()->name}}
-        </div>
-
-        <div class="card-body">
-
-            {{-- <div class="mb-3">
-            <a type="button"  href="{{route('ticketC.index')}}" class="btn btn-primary">Soporte a ticktes</a>
-
-            </div>
-
-        </div>
-
-      </div>
-
-</div> 
 
 @stop
