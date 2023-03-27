e51ac1c81e7ff6cfdd3321dbeb682f9cd55f495e
Agus737 <116747072+Agus737@users.noreply.github.com>
2023-03-27 14:33:17 -0600

    cositas

--- resources/views/MenuJefe.blade.php
+++ resources/views/MenuJefe.blade.php
@@ -1,99 +1,11 @@
-@extends('PlantillaJ')
+@extends('plantillaJ')
 
 @section('contenido')
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Macuin Dashboard</title>
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-	<!-- Custom CSS -->
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-
-	<!-- Main content -->
-
-
-	<div class="d-flex"> 
-		<div class="flex-column flex-shrink-0 p-3 text-bg-dark" class="bg-primary"style="width: 280px; height: 100vh;">
-		<a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-		  <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-		  <span class="fs-4">Macuin Dashboard</span>
-		</a>
-		<hr>
-		<ul class="nav nav-pills flex-column mb-auto">
-		  <li class="nav-item">
-			<a href="{{route('homejefe')}}" class="nav-link active" aria-current="page">
-			  <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-			  Home
-			</a>
-		  </li>
-		  <li>
-			<a href="{{route('Usuario.index')}}" class="nav-link text-white">
-			  <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-			  Administración Usuarios
-			</a>
-		  </li>
-		  <li>
-			<a href="{{route('depa.index')}}" class="nav-link text-white">
-			  <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-			  Registro Departamentos
-			</a>
-		  </li>
-		  <li>
-			<a href="{{route('ticket.index')}}" class="nav-link text-white">
-			  <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-			  Administración de Tickets
-			</a>
-		  </li>
-		  <li>
-		  </li>
-      <li>
-			<a href="{{route('ticketA.create')}}" class="nav-link text-white">
-			  <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-			  Asignar ticket
-			</a>
-		  </li>
-		  <li>
-		  </li>
-		</ul>
-		<hr>
-		<div class="dropdown">
-		  <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="{{ Auth::user()->url }}"  width="32" height="32" class="rounded-circle me-2">
-			<strong>{{Auth::user()->name}}</strong>
-		  </a>
-		  <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-			<li><a class="dropdown-item" href="#">Configuraciones</a></li>
-			<button type="button" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#ModalPerfil"> 
-				Perfil
-			 </button>
-		  </ul>
-      <div class="fixed-bottom">
-
-        <a href="{{route('Cierra.Sesion')}}">
-        <button class="btn btn-danger ms-3 mb-3"  type="button">Cerrar sesión</button>
-      </div>
-      </a>
-        </div>
-
-		</div>
-	  </div>
-	  
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
-    <!-- Font Awesome JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
  
-</body>
 
-
+@include('ModalPerfilAux')
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+	<div class="container">
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -146,50 +58,8 @@
         -webkit-overflow-scrolling: touch;
       }
     </style>
-  </head>
-
-
-  </html>
-  <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-	<div class="container">
-		<div class="row">
-  <html lang="en"><head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.108.0">
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotron/">
-
-<link href="/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
 
   <body>
-
-    @include('ModalPerfil')
-
-
-    @if(session()->has('Actualizarr'))
-    {!! 
-    " <script> 
-          Swal.fire(
-          'Muy Bien Very Good!',
-          'Usuario Editado',
-          'success'  
-    ) </script> "!!}
-    @endif
-
-    @if(session()->has('Entrar'))
-    {!! 
-    " <script> 
-          Swal.fire(
-          'Muy Bien !',
-          'Welcome to the jungle',
-          'success'  
-    ) </script> "!!}
-    @endif
-
     
   <div class="container py-4">
     <div class="p-5 mb-4 bg-light rounded-3">
@@ -224,3 +94,4 @@
 </main>
 </body>
 </html>
+@stop
